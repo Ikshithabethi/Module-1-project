067f90d50f082990aefb4bbae6e5f618f15c10c2
Bethi Ikshitha <2320030140@klh.edu.in>
2024-08-17 12:54:32 +0530

    Update LoggingFilter.java

--- FilteringHTTP/src/com/example/filter/LoggingFilter.java
+++ FilteringHTTP/src/com/example/filter/LoggingFilter.java
@@ -4,14 +4,14 @@ public class LoggingFilter implements Filter {
     @Override
     public void execute(HttpRequest request, HttpResponse response, FilterChain chain) {
         System.out.println("Logging Filter: Logging request...");
-        // Simulate logging
+     
         System.out.println("Request URL: " + request.getUrl());
         chain.doFilter(request, response);
     }
 
 	@Override
 	public void doFilter(HttpRequest request, HttpResponse response, FilterChain chain) {
-		// TODO Auto-generated method stub
+	
 		
 	}
 }
