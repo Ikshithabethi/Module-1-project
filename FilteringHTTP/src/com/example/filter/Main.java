ee4592e8210fb4e041c77a9a9fd5ebbd3d3aaf6f
Bethi Ikshitha <2320030140@klh.edu.in>
2024-08-17 12:54:14 +0530

    Update Main.java

--- FilteringHTTP/src/com/example/filter/Main.java
+++ FilteringHTTP/src/com/example/filter/Main.java
@@ -3,13 +3,13 @@ package com.example.filter;
 public class Main {
 
 	 public static void main(String[] args) {
-	        // Create the filter chain
+	       
 	        FilterChain filterChain = new FilterChain();
 	        filterChain.addFilter(new AuthenticationFilter());
 	        filterChain.addFilter(new AuthorizationFilter());
 	        filterChain.addFilter(new LoggingFilter());
 
-	        // Create a request and response
+	        
 	        HttpRequest request = new HttpRequest();
 	        request.setUrl("/example");
 	        request.addHeader("Authorization", "Bearer token");
@@ -17,10 +17,10 @@ public class Main {
 
 	        HttpResponse response = new HttpResponse();
 
-	        // Execute the filter chain
+	     
 	        filterChain.doFilter(request, response);
 
-	        // Print the final response
+	       
 	        System.out.println("Final Response Status Code: " + response.getStatusCode());
 	        System.out.println("Final Response Body: " + response.getBody());
 	    }
