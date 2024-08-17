c1bd65c8e27c3ae71c209c41635764dd0a736d01
Bethi Ikshitha <2320030140@klh.edu.in>
2024-08-17 12:57:30 +0530

    Update Main.java

--- TransportationManagementSystem/src/com/example/tms/main/Main.java
+++ TransportationManagementSystem/src/com/example/tms/main/Main.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // 1. Route Planning
+      
         RoutePlanner planner = new RoutePlanner();
         Location start = new Location(40.7128, -74.0060); // New York
         Location end = new Location(34.0522, -118.2437);  
@@ -18,12 +18,12 @@ public class Main {
         System.out.println("Optimal Route Distance: " + route.getDistance() + " km");
         System.out.println("Optimal Route Duration: " + route.getDuration() + " minutes");
 
-        // 2. Vehicle Tracking
+        
         VehicleTracker tracker = new VehicleTracker();
         VehicleLocation location = tracker.getCurrentLocation(vehicle.getId());
         System.out.println("Current Location of Vehicle: (" + location.getLatitude() + ", " + location.getLongitude() + ")");
 
-        // 3. Scheduling
+      
         Scheduler scheduler = new Scheduler();
         Schedule schedule = scheduler.createSchedule(List.of(new Delivery()), List.of(vehicle));
         System.out.println("Schedule created: " + schedule);
