8700cf11e82e2993b4e11d28a39ec130a3bd0018
Bethi Ikshitha <2320030140@klh.edu.in>
2024-08-17 12:55:57 +0530

    Update AuthorizationFilter.java

--- FilteringHTTP/src/com/example/filter/AuthorizationFilter.java
+++ FilteringHTTP/src/com/example/filter/AuthorizationFilter.java
@@ -3,8 +3,8 @@ package com.example.filter;
 public class AuthorizationFilter implements Filter {
     @Override
     public void execute(HttpRequest request, HttpResponse response, FilterChain chain) {
-        System.out.println("Authorization Filter: Checking authorization...");
-        // Simulate authorization check
+        System.out.println(" Checking authorization...");
+       
         String role = request.getHeader("Role");
         if (role == null || !role.equals("ADMIN")) {
             response.setStatusCode(403); // Forbidden
@@ -16,7 +16,7 @@ public class AuthorizationFilter implements Filter {
 
 	@Override
 	public void doFilter(HttpRequest request, HttpResponse response, FilterChain chain) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 }
