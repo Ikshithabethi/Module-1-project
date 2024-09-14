e240de2699e96064f36894af523db3e69ef47de0
Bethi Ikshitha <2320030140@klh.edu.in>
2024-09-14 13:06:00 +0530

    Delete FilteringHTTP directory

--- FilteringHTTP/src/com/example/filter/Filter.java
+++
@@ -1,7 +0,0 @@
-package com.example.filter;
-
-public interface Filter {
-	void doFilter(HttpRequest request, HttpResponse response, FilterChain chain);
-
-	void execute(HttpRequest request, HttpResponse response, FilterChain chain);
-}
